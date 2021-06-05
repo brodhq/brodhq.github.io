1256f6cb066e8fc4389819b89b4ade76458d1635
mads hargreave <mads.hargreave@gmail.com>
2021-06-05 11:12:34 +0200

    released

--- packages/website-app/pages/_app.tsx
+++ packages/website-app/pages/_app.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../ga'
 import 'tailwindcss/tailwind.css'
 import 'highlight.js/styles/github.css'
 import '../styles/global.css'
@@ -9,6 +11,24 @@ interface AppProps {
 }
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+    const router = useRouter()
+
+    const handleRouteChange = (url: string) => {
+        ga.pageview(url)
+    }
+
+    useEffect(() => {
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return <Component {...pageProps} />
 }
 
