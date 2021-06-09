fb65c14c2c1a5fe678b4a08da8dfa49e30a68a2b
madshargreave <mads.hargreave@gmail.com>
2021-06-09 12:11:48 +0000

    Deploying to gh-pages from @ kransio/krans.github.io@b93fca035c2c9dfa9f9d3201405d8cdfb24c7969 🚀

--- _next/static/IP3fpKynDfHUCo91ngtKb/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fdocs","\u002Fexamples","\u002F","\u002Fblog\u002F[slug]","\u002Fdocs\u002F[...slug]","\u002Fexamples\u002F[...slug]","\u002Fguides\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
