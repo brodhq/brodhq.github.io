cacd29548ba8fa1fc0c8616c9c233e690458ad05
mads hargreave <mads.hargreave@gmail.com>
2021-06-10 12:05:07 +0200

    released

--- packages/website-app/tailwind.config.js
+++ packages/website-app/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
         './layouts/**/*.{js,ts,jsx,tsx}',
-        './node_modules/\\@geislabs**/dist/*.js',
+        './node_modules/\\@geislabs/**/dist/**/*.js',
     ],
     darkMode: false, // or 'media' or 'class'
     // mode: 'jit',
